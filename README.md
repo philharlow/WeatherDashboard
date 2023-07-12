38198fbbf1003709197733078164be29c479cc73
phil <phil.harlow@gmail.com>
2023-07-11 20:43:27 -0700

    Another test

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-#Weather Dashboard
+# Weather Dashboard
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
 
 module.exports = {
     plugins: [
 		new Dotenv(),
+		new webpack.DefinePlugin({
+			'process.env.OPEN_WEATHER_API_KEY': "process.env.OPEN_WEATHER_API_KEY",
+		}),
         new CopyWebpackPlugin({
             patterns: [
                 { from: "public" }
